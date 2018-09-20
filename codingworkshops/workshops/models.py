341d43ded3a9fd9cde043c0ed84d6ca14bc8e506
Anton <antonoutkine@gmail.com>
2018-09-19 19:07:14 -0500

    Fix ordering

--- codingworkshops/workshops/models.py
+++ codingworkshops/workshops/models.py
@@ -3,18 +3,27 @@ from codingworkshops.users.models import User
 
 
 class Workshop(models.Model):
+    class Meta:
+        ordering = ['pk']
+
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=500)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Lesson(models.Model):
+    class Meta:
+        ordering = ['pk']
+
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100, blank=True)
     workshop = models.ForeignKey(Workshop, on_delete=models.CASCADE)
 
 
 class Slide(models.Model):
+    class Meta:
+        ordering = ['pk']
+
     name = models.CharField(max_length=50)
     description = models.TextField()
     starting_code = models.TextField(default='')
