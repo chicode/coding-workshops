8ec0ee8a6749b3fbf8803d4a373a044becc3b7df
Anton <antonoutkine@gmail.com>
2018-10-18 08:04:32 -0500

    Make usernames case insensitive

--- codingworkshops/users/models.py
+++ codingworkshops/users/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.contrib.auth.validators import ASCIIUsernameValidator
 
 RESERVED_USERNAMES = ['enter', 'signup', 'about']
+
+
+class CustomUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        case_insensitive_username_field = '{}__iexact'.format(
+            self.model.USERNAME_FIELD
+        )
+        return self.get(**{case_insensitive_username_field: username})
 
 
 def username_validator(username):
@@ -10,6 +18,8 @@ def username_validator(username):
 
 
 class User(AbstractUser):
+    objects = CustomUserManager()
+
     username_validators = [ASCIIUsernameValidator(), username_validator]
 
     bio = models.CharField(max_length=500, blank=True)
